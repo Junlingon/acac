8b9c8377ecb07916d1989fa8a72086e4b57168e2
JunlinGong <913756523@qq.com>
2023-11-25 23:20:53 +0800

    feat: add Scj initalization

---
+++ task/src/23-11-25-收藏集榜单/index.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+
+function App() {
+    const [count, setCount] = useState(0)
+
+    return (
+        <>
+            <div>
+                46465
+            </div>
+        </>
+    )
+}
+
+export default App
