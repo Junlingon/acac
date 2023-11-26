933aa5085139671db6e6728d6e2979df642fee6a
JunlinGong <913756523@qq.com>
2023-11-26 22:44:03 +0800

    feat: add btn css

--- task/src/23-11-25-收藏集榜单/TabButton.tsx
+++ task/src/23-11-25-收藏集榜单/TabButton.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import { Button, Flex } from 'antd';
 
+import './button.css';
+
 const TabButton: React.FC = () => (
-    <Flex gap="small" wrap="nowrap" justify='center'>
-        <Button type="primary">最受欢迎</Button>
-        <Button type="primary">粉丝呼声最高</Button>
-        <Button type="primary">最具潜力</Button>
-    </Flex>
+    <>
+        <Flex gap="small" wrap="nowrap" justify='center'>
+            <Button type="primary">最受欢迎</Button>
+            <Button type="primary">粉丝呼声最高</Button>
+            <Button type="primary">最具潜力</Button>
+        </Flex>
+        <Flex gap="small" wrap="nowrap" justify='center'>
+            <Button type="primary">单人</Button>
+            <Button type="primary">团体</Button>
+        </Flex>
+    </>
 );
 
 export default TabButton;
