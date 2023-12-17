32e1fa1cd9753a1afc95115fe9fcb90005888f47
JunlinGong <913756523@qq.com>
2023-12-17 23:24:17 +0800

    feat: 2

--- task/src/23-12-06-quantai/src/client/foot/Foot.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-import styles from './foot.module.less'
-
-const Foot = () => {
-  return <div className={styles.foot}></div>
-}
-
-export default Foot
--- task/src/23-12-06-quantai/src/jotai/home.tsx
+++
@@ -1,8 +0,0 @@
-
-import { atom } from 'jotai'
-
-export const isCheckedAtom = atom<any>(false)
-
-export const buyPropsAtom = atom<any>(null)
-
-export const isReportAtom = atom<any>(false)
